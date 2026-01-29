fb2c281928e28d4d7b68e96b002a81e9820671be
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-01-29 17:24:18 -0600

    setting up spring boot for java backend

---
+++ laliga2024_2025/src/main/java/com/laliga/laliga2024_2025/Laliga20242025Application.java
@@ -0,0 +1,13 @@
+package com.laliga.laliga2024_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laliga20242025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laliga20242025Application.class, args);
+	}
+
+}
---
+++ laliga2024_2025/src/test/java/com/laliga/laliga2024_2025/Laliga20242025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laliga.laliga2024_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laliga20242025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
