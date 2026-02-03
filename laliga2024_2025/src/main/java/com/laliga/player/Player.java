030df2be37d2af7a3de370b09a7e8e2129af97fd
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-02-02 19:56:31 -0600

    updated the folder typo name

---
+++ laliga2024_2025/src/main/java/com/laliga/player/Player.java
@@ -0,0 +1,10 @@
+package com.laliga.player;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+}
--- laliga2024_2025/src/main/java/com/laliga/player/player.java
+++
@@ -1,6 +0,0 @@
-package com.laliga.player; //The reason we made a folder name player is to keep all player related classes in one place and when calling them we can easily import them from this folder.
-
-@Entity
-public class player {
-
-}
