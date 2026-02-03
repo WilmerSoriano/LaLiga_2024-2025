6f0b287aa4ce680ae59e038889cce4480bfa7c10
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-02-02 18:29:09 -0600

    creating the player base information

---
+++ laliga2024_2025/src/main/java/com/laliga/player/player.java
@@ -0,0 +1,6 @@
+package com.laliga.player; //The reason we made a folder name player is to keep all player related classes in one place and when calling them we can easily import them from this folder.
+
+@Entity
+public class player {
+
+}
