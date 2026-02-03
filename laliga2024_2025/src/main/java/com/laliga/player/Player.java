9fdd46b0c76173be2ecbf0715d1e1438d0278d24
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-02-02 20:13:09 -0600

    Added the constructor, Entity and primary key with attriubutes

--- laliga2024_2025/src/main/java/com/laliga/player/Player.java
+++ laliga2024_2025/src/main/java/com/laliga/player/Player.java
@@ -1,11 +1,46 @@
-package com.laliga.player;
+package com.laliga.player; // The package declaration indicates that this class is part of the com.laliga.player folder
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "players")
+@Entity // To mark this class as a JPA entity, meaning it will be mapped to a database table
+@Table(name = "players") // Specifies the name of the database table to which this entity is mapped (yes my database table is called players)
 public class Player {
 
+    @Id // Tells JPA(Java Persistence API) that this field is the primary key
+    @Column(name = "id") 
     private int id;
+    private String name;
+    private String nation;
+    private String position;
+    private String team;
+    private int age;
+    private int matches;
+    private int min;
+    private int goals;
+    private int assists;
+    private int yellowCards;
+    private int redCards;
+
+    public Player(int id, String name, String nation, String position, String team, int age, int matches, int min, int goals, int assists, int yellowCards, int redCards) {
+        this.id = id;
+        this.name = name;
+        this.nation = nation;
+        this.position = position;
+        this.team = team;
+        this.age = age;
+        this.matches = matches;
+        this.min = min;
+        this.goals = goals;
+        this.assists = assists;
+        this.yellowCards = yellowCards;
+        this.redCards = redCards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
