aee5cd3db3c056070392a022649fe9db47489509
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-02-04 20:10:40 -0600

    Added notes to remember why Repostiory is important to java and SQL

--- laliga2024_2025/src/main/java/com/laliga/player/PlayerRepository.java
+++ laliga2024_2025/src/main/java/com/laliga/player/PlayerRepository.java
@@ -3,9 +3,25 @@ package com.laliga.player;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository; // JpaRepository provides JPA  with related methods e.g CRUD operations
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;            // Which allowes me to talk with mySQL database    
 
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Integer> { // Creates a database access layer for the Player table/entity.
     Optional<Player> findByName(String name); // The optional type is used to represent a value that may or may not be present in the database
 }
+
+
+/*
+Your code
+ ||
+ \/
+JpaRepository
+ ||
+ \/
+Hibernate (converts Java to SQL)
+ ||
+ \/
+MySQL
+
+* I don't need to write any SQL queries myself because JpaRepo provides methods like findAll(), save(), deleteById(), etc.
+*/
