75b8b0719a7431d4404eb678b4eebe3c843cab45
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-02-17 19:22:35 -0600

    Testing the waters AKA: Seeing how react and typescript works

--- laliga-frontend/src/App.tsx
+++ laliga-frontend/src/App.tsx
@@ -9,25 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>LaLiga Players 2024-2025</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Testing testing</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
